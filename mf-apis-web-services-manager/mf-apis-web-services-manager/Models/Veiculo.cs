b6016b0b4fea99bc02f5ebe1f2160ef298b2af8c
Matheus Fernandes <mfernandesr97@gmail.com>
2023-09-13 15:39:21 -0300

    aula 01 modelagem e configuração de dados

---
+++ mf-apis-web-services-manager/mf-apis-web-services-manager/Models/Veiculo.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_manager.Models
+{
+    [Table ("Veículos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Marca { get; set; }
+
+        [Required]
+        public string Modelo { get; set; }
+
+        [Required]
+        public string Placa { get; set; }
+
+        [Required]
+        public int AnoFabricacao { get; set; }
+
+        [Required]
+        public int AnoModelo { get; set; }
+
+        public ICollection <Consumo> Consumos { get; set; }
+    }
+}
