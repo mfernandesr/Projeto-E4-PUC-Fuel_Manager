52f82109e498864145605a2bca3cb349fb10605b
Matheus Fernandes <mfernandesr97@gmail.com>
2023-09-15 14:48:52 -0300

    Aula 01b Tema 02 Criando as ações Create e GetById

--- mf-apis-web-services-manager/mf-apis-web-services-manager/Controllers/VeiculosController.cs
+++ mf-apis-web-services-manager/mf-apis-web-services-manager/Controllers/VeiculosController.cs
@@ -23,5 +23,29 @@ namespace mf_apis_web_services_manager.Controllers
 
             return Ok(model);
         }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(Veiculo model)
+        {
+            if (model.AnoFabricacao <= 0 || model.AnoModelo <= 0)
+            {
+                return BadRequest(new {message = "Ano de Fabricação e Ano de Modelo são obrigatorios e devem ser maiores do que zero"});
+            }
+            
+            _context.Veiculos.Add(model);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new {id = model.Id}, model);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetById(int id)
+        {
+            var model = await _context.Veiculos.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (model == null) NotFound();
+
+            return Ok(model);
+        }
     }
 }
