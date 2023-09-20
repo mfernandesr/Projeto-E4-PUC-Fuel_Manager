770da195add7c9b94d8a2226cd6d37b038f4a9be
Matheus Fernandes <mfernandesr97@gmail.com>
2023-09-20 09:42:04 -0300

    Aula 03c Tema 2 Criação dos serviços para associar Usuários aos Veículos

--- mf-apis-web-services-manager/mf-apis-web-services-manager/Controllers/VeiculosController.cs
+++ mf-apis-web-services-manager/mf-apis-web-services-manager/Controllers/VeiculosController.cs
@@ -41,7 +41,10 @@ namespace mf_apis_web_services_manager.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult> GetById (int id)
         {
-            var model = await _context.Veiculos.Include(t => t.Consumos).FirstOrDefaultAsync(c => c.Id == id);
+            var model = await _context.Veiculos
+                .Include(t => t.Usuarios).ThenInclude(t => t.Usuario)
+                .Include(t => t.Consumos)
+                .FirstOrDefaultAsync(c => c.Id == id);
 
             if (model == null) return NotFound();
 
@@ -83,6 +86,32 @@ namespace mf_apis_web_services_manager.Controllers
             model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "self", metodo: "GET"));
             model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "update", metodo: "PUT"));
             model.Links.Add(new LinkDto(model.Id, Url.ActionLink(), rel: "delete", metodo: "DELETE"));
+        }
+
+        [HttpPost("{id}/usuarios")]
+        public async Task<ActionResult> AddUsuario(int id, VeiculoUsuarios model)
+        {
+            if (id != model.VeiculoId) return BadRequest();
+
+            _context.VeiculosUsuario.Add(model);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetById", new {id = model.VeiculoId}, model);
+        }
+
+        [HttpDelete("{id}/usuarios/{usuarioId}")]
+        public async Task<ActionResult> DeleteUsuario(int id, int usuarioId)
+        {
+            var model = await _context.VeiculosUsuario
+                .Where(c => c.VeiculoId == id && c.UsuarioId == usuarioId)
+                .FirstOrDefaultAsync();
+
+            if (model == null) return NotFound();
+
+            _context.VeiculosUsuario .Remove(model);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
